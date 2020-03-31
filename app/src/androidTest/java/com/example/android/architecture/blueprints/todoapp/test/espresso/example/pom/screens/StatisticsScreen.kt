4c6065c7bc050f3e8e1f30689a27e2d9bdf7f5c7
jcekala <jakubcekala1@gmail.com>
2020-03-31 22:18:55 +0200

    Add simple implementation of methods for StatisticScreen

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/espresso/example/pom/screens/StatisticsScreen.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/espresso/example/pom/screens/StatisticsScreen.kt
@@ -1,5 +1,42 @@
 package com.example.android.architecture.blueprints.todoapp.test.espresso.example.pom.screens
 
-class StatisticsScreen {
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.matcher.ViewMatchers.*
+import com.example.android.architecture.blueprints.todoapp.R
+import com.example.android.architecture.blueprints.todoapp.test.chapter3.checkDisplayed
+import com.example.android.architecture.blueprints.todoapp.test.chapter3.checkNotDisplayed
+import com.example.android.architecture.blueprints.todoapp.test.chapter3.click
+import org.hamcrest.CoreMatchers.allOf
 
+class StatisticsScreen {
+    private val okDialogButton = onView(allOf(withId(android.R.id.button1), withText(android.R.string.ok)))
+    private val cancelDialogButton = onView(allOf(withId(android.R.id.button2), withText(android.R.string.cancel)))
+    private val alertTitle = onView(withId(R.id.alertTitle))
+    private val alertMessageTextView = onView(withId(android.R.id.message))
+
+    private val statisticTextView = onView(allOf(withId(R.id.statistics), withText(R.string.statistics_no_tasks)))
+    private val statisticHeader = onView(allOf(withParent(withId(R.id.toolbar)), withText(R.string.statistics_title)))
+
+    fun clickOkAlertDialog() {
+        okDialogButton.click()
+    }
+
+    fun clickCancelAlertDialog() {
+        cancelDialogButton.click()
+    }
+
+    fun checkAlertDialogDisplay() {
+        alertTitle.checkDisplayed()
+        alertMessageTextView.checkDisplayed()
+    }
+
+    fun checkAlertDialogNotDisplayed() {
+        alertTitle.checkNotDisplayed()
+        alertMessageTextView.checkNotDisplayed()
+    }
+
+    fun verifyStatisticScreen() {
+        statisticTextView.checkDisplayed()
+        statisticHeader.checkDisplayed()
+    }
 }

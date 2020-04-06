b56b5ce8b37208276d9c7539a438e192a65f9427
Jakub Cekała <jakubcekala1@gmail.com>
2020-04-06 21:20:34 +0200

    Simple implementation of NotificationSettingsScreen test methods

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/espresso/example/pom/screens/settings/NotificationSettingsScreen.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/espresso/example/pom/screens/settings/NotificationSettingsScreen.kt
@@ -1,5 +1,109 @@
 package com.example.android.architecture.blueprints.todoapp.test.espresso.example.pom.screens.settings
 
-class NotificationSettingsScreen : SettingBaseScreen() {
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.*
+import com.example.android.architecture.blueprints.todoapp.R
+import org.hamcrest.CoreMatchers.allOf
+import java.lang.Exception
 
+open class NotificationSettingsScreen : SettingBaseScreen() {
+    private val sendNotificationButton = onView(allOf(withId(android.R.id.title), withText(R.string.pref_title_send_notifications)))
+    private val sendNotificationButtonWithDelayButton = onView(allOf(withId(android.R.id.title), withText(R.string.pref_title_send_notifications_with_delay)))
+    private val enableNotificationButton = onView(allOf(withId(android.R.id.title), withText(R.string.pref_title_new_message_notifications)))
+    private val enableNotificationSwitch = onView(allOf(withId(android.R.id.switch_widget), withParent(withId(android.R.id.widget_frame))))
+
+    fun clickOnSendNotificationButton() {
+        sendNotificationButton.perform(click())
+    }
+
+    fun clickOnSendNotificationWIthDelayButton() {
+        sendNotificationButtonWithDelayButton.perform(click())
+    }
+
+    fun enableNotification(): EnableNotificationOnNotificationSettingsScreen {
+        return if(isEnableNotificationSubmenuDisplayed()) {
+            EnableNotificationOnNotificationSettingsScreen()
+        } else {
+            enableNotificationButton.perform(click())
+            EnableNotificationOnNotificationSettingsScreen()
+        }
+    }
+
+    private fun isEnableNotificationSubmenuDisplayed(): Boolean {
+        return try {
+            onView(allOf(withId(android.R.id.title), withText(R.string.slider_title))).check(matches(isDisplayed()))
+            onView(allOf(withId(android.R.id.title), withText(R.string.pref_title_ringtone))).check(matches(isDisplayed()))
+            onView(allOf(withId(android.R.id.title), withText(R.string.pref_title_vibrate))).check(matches(isDisplayed()))
+            true
+        } catch (exception: Exception) {
+            false
+        }
+    }
+
+    class EnableNotificationOnNotificationSettingsScreen : NotificationSettingsScreen() {
+        private val notifyWhenButton = onView(allOf(withId(android.R.id.title), withText(R.string.slider_title)))
+        private val ringtoneButton = onView(allOf(withId(android.R.id.title), withText(R.string.pref_title_ringtone)))
+        private val vibrateButton = onView(allOf(withId(android.R.id.title), withText(R.string.pref_title_vibrate)))
+        private val vibrateCheckbox = onView(allOf(withId(android.R.id.checkbox), withParent(withId(android.R.id.widget_frame))))
+
+        fun clickOnNotifyWhenButton(): NotificationOlderDialog {
+            notifyWhenButton.perform(click())
+            return NotificationOlderDialog()
+        }
+
+        fun clickOnRingtoneButton(): RingtoneDialog {
+            ringtoneButton.perform(click())
+            return RingtoneDialog()
+        }
+
+        fun turnOnVibrate() {
+            if (isVibrateCheckboxChecked()) {
+                return
+            } else {
+                vibrateButton.perform(click())
+            }
+        }
+
+        private fun isVibrateCheckboxChecked(): Boolean {
+            return try {
+                vibrateCheckbox.check(matches(isChecked()))
+                true
+            } catch (exception: Exception) {
+                false
+            }
+        }
+
+        inner class NotificationOlderDialog {
+            private val sliderSeekbar = onView(withId(R.id.slider_preference_seekbar))
+            private val okButton = onView(allOf(withId(android.R.id.button1), withText(android.R.string.ok)))
+            private val cancelButton = onView(allOf(withId(android.R.id.button2), withText(android.R.string.cancel)))
+
+            fun clickOnOkButton(): EnableNotificationOnNotificationSettingsScreen {
+                okButton.perform(click())
+                return EnableNotificationOnNotificationSettingsScreen()
+            }
+
+            fun clickOnCancelButton(): EnableNotificationOnNotificationSettingsScreen {
+                cancelButton.perform(click())
+                return EnableNotificationOnNotificationSettingsScreen()
+            }
+        }
+
+        inner class RingtoneDialog {
+            private val okButton = onView(allOf(withId(android.R.id.button1), withText(android.R.string.ok)))
+            private val cancelButton = onView(allOf(withId(android.R.id.button2), withText(android.R.string.cancel)))
+
+            fun clickOnOkButton(): EnableNotificationOnNotificationSettingsScreen {
+                okButton.perform(click())
+                return EnableNotificationOnNotificationSettingsScreen()
+            }
+
+            fun clickOnCancelButton(): EnableNotificationOnNotificationSettingsScreen {
+                cancelButton.perform(click())
+                return EnableNotificationOnNotificationSettingsScreen()
+            }
+        }
+    }
 }

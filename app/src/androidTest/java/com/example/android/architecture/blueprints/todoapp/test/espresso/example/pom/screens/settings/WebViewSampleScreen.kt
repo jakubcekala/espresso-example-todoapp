3e1a146f4c423c6abb958a7c7591eed4a91cdde2
Jakub Cekała <jakubcekala1@gmail.com>
2020-04-06 21:23:42 +0200

    Add web view in WebViewSampleScreen.kt

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/espresso/example/pom/screens/settings/WebViewSampleScreen.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/espresso/example/pom/screens/settings/WebViewSampleScreen.kt
@@ -1,5 +1,9 @@
 package com.example.android.architecture.blueprints.todoapp.test.espresso.example.pom.screens.settings
 
-class WebViewSampleScreen : SettingBaseScreen() {
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import com.example.android.architecture.blueprints.todoapp.R
 
+class WebViewSampleScreen : SettingBaseScreen() {
+    private val webView = onView(withId(R.id.web_view))
 }

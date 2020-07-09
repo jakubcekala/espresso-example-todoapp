c3343a36b1b28b3da3d90af196cb7b7b341a994d
Jakub Cekała <jakubcekala1@gmail.com>
2020-07-09 14:08:11 +0200

    Change test data for TasksOrderTests.kt

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/espresso/example/tests/TasksOrderTests.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/test/espresso/example/tests/TasksOrderTests.kt
@@ -1,6 +1,8 @@
 package com.example.android.architecture.blueprints.todoapp.test.espresso.example.tests
 
 import com.example.android.architecture.blueprints.todoapp.test.espresso.example.pom.screens.TodoListScreen
+import com.example.android.architecture.blueprints.todoapp.test.espresso.example.testdata.TaskModel
+import com.example.android.architecture.blueprints.todoapp.test.espresso.example.testdata.TasksTestData
 import org.junit.Before
 import org.junit.Test
 import java.util.*
@@ -8,44 +10,39 @@ import java.util.*
 class TasksOrderTests: BaseTest() {
 
     lateinit var todoListScreen: TodoListScreen
-
-    private var taskTitle1 = ""
-    private var taskTitle2 = ""
-    private var taskDesc1 = ""
-    private var taskDesc2 = ""
+    lateinit var taskItem1: TaskModel
+    lateinit var taskItem2: TaskModel
 
     @Before
     fun setTasksData() {
-        taskTitle1 = "Title1" + Random().nextInt()
-        taskTitle2 = "Title2" + Random().nextInt()
-        taskDesc1 = "Description1" + Random().nextInt()
-        taskDesc2 = "Description2" + Random().nextInt()
+        taskItem1 = TasksTestData.getTaskItem()
+        taskItem2 = TasksTestData.getTaskItem()
     }
 
     @Before
     fun createTwoTasks() {
-        todoListScreen = TodoListScreen().clickOnFABButton().addNewTask(taskTitle1, taskDesc1).
+        todoListScreen = TodoListScreen().clickOnFABButton().addNewTask(taskItem1.title, taskItem1.description).
                 waitForSnackbarDisappearing().
-                clickOnFABButton().addNewTask(taskTitle2, taskDesc2)
+                clickOnFABButton().addNewTask(taskItem2.title, taskItem2.description)
     }
 
     @Test
     fun activeTasksFilter() {
-        todoListScreen.markTaskAsCompleted(taskTitle1).showActiveTasks().
-                checkIfTaskIsDisplayed(taskTitle2).checkIfTaskIsNotDisplayed(taskTitle1)
+        todoListScreen.markTaskAsCompleted(taskItem1.title).showActiveTasks().
+                checkIfTaskIsDisplayed(taskItem2.title).checkIfTaskIsNotDisplayed(taskItem1.title)
     }
 
     @Test
     fun completedTaskFilter() {
-        todoListScreen.markTaskAsCompleted(taskTitle1).showCompletedTasks().
-                checkIfTaskIsDisplayed(taskTitle1).checkIfTaskIsNotDisplayed(taskTitle2)
+        todoListScreen.markTaskAsCompleted(taskItem1.title).showCompletedTasks().
+                checkIfTaskIsDisplayed(taskItem1.title).checkIfTaskIsNotDisplayed(taskItem2.title)
     }
 
     @Test
     fun allTasksFilter() {
-        todoListScreen.markTaskAsCompleted(taskTitle1).showAllTasks().
-                checkIfTaskIsDisplayed(taskTitle1).checkIfTaskIsDisplayed(taskTitle2).
-                markTaskAsCompleted(taskTitle2).
-                checkIfTaskIsDisplayed(taskTitle1).checkIfTaskIsDisplayed(taskTitle2)
+        todoListScreen.markTaskAsCompleted(taskItem1.title).showAllTasks().
+                checkIfTaskIsDisplayed(taskItem1.title).checkIfTaskIsDisplayed(taskItem2.title).
+                markTaskAsCompleted(taskItem2.title).
+                checkIfTaskIsDisplayed(taskItem1.title).checkIfTaskIsDisplayed(taskItem2.title)
     }
 }
